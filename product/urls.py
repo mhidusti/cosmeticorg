346fc7c5f97fe2ce6d20cd9ab44c55e7db4810ea
fatemedavoodi <bahare.khazan98@gmail.com>
2024-02-10 22:57:38 +0330

    productt

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name='product'),
-    path('', views.ProductDetailView.as_view(), name='product-detail'),
+    path('', views.ProductDetailView.as_view(), name='detail'),
     
 ]
