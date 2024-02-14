88871897b976c29e9a96a449de3bf78e9709b78c
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-02-14 20:18:12 +0330

    migrate

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AdminServices(admin.ModelAdmin):
+
+    list_display = ['title', 'content', 'status']
+    list_filter = ['status']
+    search_fields = ['title']
+
+admin.site.register(Products)
+admin.site.register(Category)
