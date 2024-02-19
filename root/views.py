0f1d5ac8d35f72e9e7de77d1d25b9abdc243ce56
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-02-19 11:31:16 +0330

    index

--- root/views.py
+++ root/views.py
@@ -10,33 +10,6 @@ from .forms import ContactUsForm,CommentForm
 
 
 
-# def home(request):
-#     # if request.method == 'GET':
-#     #     category = Category.objects.all()
-#     #     productFeature = Product_Feature.objects.filter(status = True)
-#     #     bestproducts = Best_Products.objects.filter(status = True)
-#     #     products = Products.objects.filter(status = True)
-#     #     special = Special_Products.objects.filter(status = True)
-#     #     pack = Pack_Products.objects.filter(status = True)
-#     #     gallery = Gallery.objects.all()
-#     #     social= Social.objects.filter(status = True)
-#     #     context = {
-#     #          'category': category,
-#     #          'pf': productFeature,
-#     #          'bp': bestproducts,
-#     #          'products': products,
-#     #          'special': special,
-#     #          'pack': pack,
-#     #          'gallery': gallery,
-#     #          'social': social,
-            
-#     #         }
-#     return render(request,"root/index.html")
-
-# def about (request):
-#     return render(request,"root/about.html")
-
-
 class HomeListView(ListView):
     template_name = "root/index.html"
     context_object_name = 'product_feature'
@@ -59,16 +32,7 @@ class HomeListView(ListView):
             messages.add_message(request,messages.ERROR,'Invalid data please try again')
             return redirect('root:home')
 
-    # def get_context_data(self, **kwargs: Any): 
-    #     context = super().get_context_data(**kwargs)
-    #     context['gallery'] = Gallery.objects.filter(status = True)
-    #     return context
-    
-    # def get_context_data(self, **kwargs: Any): 
-    #     context = super().get_context_data(**kwargs)
-    #     context['social'] = Social.objects.filter(status = True)
-    #     return context
-
+   
 
 
 
@@ -97,20 +61,6 @@ class ContactListView(ListView):
         
 
 
-
-
-# def contact(request):
-#     if request.method == 'POST':
-#         form = ContactUsForm(request.POST)
-#         if form.is_valid():
-#             form.save()  
-#             messages.add_message(request,messages.SUCCESS,'we received your message and call with you as soon')
-#             return redirect('root:contact')   
-#         else :
-#             messages.add_message(request,messages.ERROR,'Invalid data')
-#             return redirect('root:contact')
-
-#     return render(request,"root/contact.html")
 
 
 
